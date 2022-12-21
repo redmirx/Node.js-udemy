beff29d9265ee68abfb1e59bc45a12d71c9c53df
redmirx <redmirx9@gmail.com>
2022-12-21 22:27:09 +0500

    simple web server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+
+//////////////////////////////////////////////////////
+///////////  FILES
 
 // Blocking, synchronous way
 
@@ -10,15 +14,27 @@ const fs = require("fs");
 
 // Non-blocking, asynchronous way
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written 😄");
-      });
-    });
-  });
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written 😄");
+//       });
+//     });
+//   });
+// });
+// console.log("This line will be exicuted first");
+
+//////////////////////////////////////////////////////
+///////////  SERVER
+
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  res.end("Hello from the server!");
 });
-console.log("This line will be exicuted first");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
