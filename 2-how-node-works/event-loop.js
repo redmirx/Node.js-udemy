9593d17dc40258d220a4f5790a5b3abf5b9e5efe
redmirx <redmirx9@gmail.com>
2022-12-26 21:09:57 +0500

    event-loop

---
+++ 2-how-node-works/event-loop.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+// process.env.UV_THREADPOOL_SIZE = 1; // this line of code will only work in a script key in package.json file
+
+setTimeout(() => {
+  console.log('Timer 1 finished');
+}, 0);
+setImmediate(() => {
+  console.log('Immediate 1 finished');
+});
+
+fs.readFile('./test-file.txt', 'utf-8', () => {
+  console.log('I/O finished');
+  console.log('-------------------');
+  setTimeout(() => {
+    console.log('Timer 2 finished');
+  }, 3000);
+  setImmediate(() => {
+    console.log('Immediate 2 finished');
+  });
+  process.nextTick(() => {
+    console.log('Process nextTick finished');
+  });
+
+  // crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  // console.log(Date.now() - start, 'Password encrypted');
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password encrypted');
+  });
+});
+
+console.log('Hello from the top lever code');
