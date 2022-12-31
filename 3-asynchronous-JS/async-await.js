9255d4d2c549cb8fc82355631ee361cbfb01cb0c
redmirx <redmirx9@gmail.com>
2022-12-31 16:56:08 +0500

    async/await

--- 3-asynchronous-JS/async-await.js
+++ 3-asynchronous-JS/async-await.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-// const superagent = require('superagent');
+const superagent = require('superagent');
 
 const readFilePro = (file) => {
   return new Promise((resolve, reject) => {
@@ -10,6 +10,28 @@ const readFilePro = (file) => {
   });
 };
 
-const getDogPic = async () => {
-  const data = await readFilePro('./dog.txt');
+const writeFilePro = (pathaname, filename) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(pathaname, filename, (err) => {
+      if (err) {
+        reject('An error occured while writing file 😥');
+      }
+      resolve('Dog image successfully saved to file!');
+    });
+  });
 };
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro('./dog.txt');
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const file = await writeFilePro(`${data}-image.txt`, res.body.message);
+    console.log(file);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+getDogPic();
