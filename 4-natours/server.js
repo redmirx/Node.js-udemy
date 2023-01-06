b23134711e75843d178517832ba81765d3dcc1fc
redmirx <redmirx9@gmail.com>
2023-01-06 20:11:45 +0500

    env variables

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,4 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+console.log(process.env.PORT);
 
 const port = 3000;
 app.listen(port, () => {
